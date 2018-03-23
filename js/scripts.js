6d5775163772d41dceb3f126d9901e1346c701a4
Matt Miller <matt.f.miller@live.com>
2018-03-23 10:03:28 -0700

    Define variables for form inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,21 @@ $(document).ready(function() {
     $("#php").hide();
     $("#ruby").hide();
 
+    var inputName = $("#name").val();
+    var inputFrontendBackend = $("input:radio[name=frontend-backend]:checked").val();
+    var inputMobileDesktop = $("#mobile-desktop").val().charAt(0);
+    var inputMacPc = $("#mac-pc").val().charAt(0);
+    var inputBackendSize = $("#backend-size").val().charAt(0);
+    var inputBackendPace = $("#backend-pace").val().charAt(0);
+
+
+    console.log(inputName);
+    console.log(inputFrontendBackend);
+    console.log(inputMobileDesktop);
+    console.log(inputMacPc);
+    console.log(inputBackendSize);
+    console.log(inputBackendPace);
+
     event.preventDefault();
   });
 });
